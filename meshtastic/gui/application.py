ba3722d914e5c7d1ba95018e6a8f064a9f7e2b55
flockofsparrows <flockofsparrows@riseup.net>
2025-07-04 18:13:49 -0400

    Complete serial connection logic

--- meshtastic/gui/application.py
+++ meshtastic/gui/application.py
@@ -1,7 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox as tkmsg
+from typing import *
 
 import meshtastic
+from meshtastic.mesh_interface import MeshInterface
+from meshtastic.serial_interface import SerialInterface
+from meshtastic.ble_interface import BLEInterface
+from meshtastic.tcp_interface import TCPInterface
 
 from pubsub import pub
 
@@ -25,31 +31,30 @@ class DeviceList(ttk.LabelFrame):
     def __init__(self, master: tk.Tk):
         super().__init__(master, text="Meshtastic Devices", labelanchor="n")
 
-        self.refresh_button: ttk.Button = ttk.Button(self, text="Add Device", command=self.add_device)
+        self.refresh_button: ttk.Button = ttk.Button(self, text="Add Device",
+                                                     command=lambda: DeviceConnectDialog(self))
         self.y_scroll: ttk.Scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL)
         self.list: ttk.Treeview = ttk.Treeview(
             self, selectmode=tk.BROWSE, yscrollcommand=self.y_scroll.set,
-            columns=("device", "name", "status", "ctype"),
-            displaycolumns=("name", "status", "ctype")
+            columns=("device", "status", "ctype", "addr"),
+            displaycolumns=("status", "ctype", "addr")
         )
         self.y_scroll["command"] = self.list.yview
-        self.list.heading("name", text="Name", anchor=tk.E)
+        self.list.heading("#0", text="Device", anchor=tk.CENTER)
         self.list.heading("status", text="Status", anchor=tk.CENTER)
-        self.list.heading("ctype", text="Interface", anchor=tk.E)
+        self.list.heading("ctype", text="Interface", anchor=tk.CENTER)
+        self.list.heading("addr", text="Address", anchor=tk.CENTER)
 
         self.refresh_button.pack(side="top", fill="x", expand=1)
         self.y_scroll.pack(side="right", fill="y", expand=1)
         self.list.pack(side="left", fill="both", expand=1)
 
-    def add_device(self):
-        dialog: DeviceConnectDialog = DeviceConnectDialog(self)
-
-        self.list.insert("", "end", values=())
+    def add_device(self, device: MeshInterface, ctype: str, addr: str):
+        self.list.insert("", "end", text=device.getLongName(), values=(device, "Connected", ctype, addr))
 
 
 class CustomDialog(tk.Toplevel):
     """Convenience base class for custom dialog windows"""
-
     def __init__(self, parent, title):
         super().__init__()
         self.transient(parent)
@@ -59,46 +64,9 @@ class CustomDialog(tk.Toplevel):
 class DeviceConnectDialog(CustomDialog):
     """User dialog to connect to a meshtastic device"""
 
-    class SerialInterfaceWidget(ttk.Frame):
-        def __init__(self, parent):
-            super().__init__(parent)
-            self.ports_list_scroll: ttk.Scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL)
-            self.ports_list: ttk.Treeview = ttk.Treeview(
-                self, selectmode=tk.BROWSE, yscrollcommand=self.ports_list_scroll.set, show="tree"
-            )
-            self.ports_list_scroll["command"] = self.ports_list.yview
-            self.ports_list.bind("<<TreeViewSelect>>", self.validate_selection)
-            self.selection_valid: bool = False
-
-        def validate_selection(self):
-            selection: tuple[str] = self.ports_list.selection()
-            self.selection_valid: bool = len(selection) == 1 and self.ports_list.tag_has("port", selection[0])
-
-        def on_active(self):
-            print("Serial connection dialog active")
-            print(f"Searching for any of VIDs: {meshtastic.util.get_unique_vendor_ids()}")
-            self.ports_list.delete(*self.ports_list.get_children())
-            for device in meshtastic.util.detect_supported_devices():
-                print(f"Found serial device: {device.name}")
-                iid: str = self.ports_list.insert(
-                    "", tk.END, text=f"{device.name} ({device.device_class})", tags=("device",), open=True
-                )
-                for port in meshtastic.util.active_ports_on_supported_devices((device,), True):
-                    self.ports_list.insert(iid, tk.END, text=port, tags=("port",))
-
-            self.ports_list.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
-            self.ports_list_scroll.pack(side=tk.RIGHT, fill=tk.Y, expand=1)
-
-    class BLEInterfaceWidget(ttk.Frame):
-        pass
-
-    class TCPInterfaceWidget(ttk.Frame):
-        pass
-
-
     def __init__(self, parent):
         super().__init__(parent, "Add Meshtastic Device")
-
+        self.parent = parent
         self.device_ctype: tk.StringVar = tk.StringVar(value="serial")
         self.ctype_frame: ttk.LabelFrame = ttk.LabelFrame(self, text="Connection Interface")
         self.ctype_buttons: Tuple[ttk.Radiobutton] = (
@@ -137,4 +105,57 @@ class DeviceConnectDialog(CustomDialog):
         self.current_interface_widget.on_active()
 
     def do_connect(self):
+        interface, addr = self.current_interface_widget.connect_device()
+        if interface is not None:
+            self.parent.add_device(interface, self.current_interface_widget.ctype, addr)
+            self.destroy()
+        else:
+            tkmsg.showwarning("Could Not Connect", "Failed to connect to the selected device.", parent=self)
+
+    class SerialInterfaceWidget(ttk.Frame):
+        ctype: str = "Serial/USB"
+        def __init__(self, parent):
+            super().__init__(parent)
+            self.ports_list_scroll: ttk.Scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL)
+            self.ports_list: ttk.Treeview = ttk.Treeview(
+                self, selectmode=tk.BROWSE, yscrollcommand=self.ports_list_scroll.set, show="tree"
+            )
+            self.ports_list_scroll["command"] = self.ports_list.yview
+            #self.ports_list.bind("<<TreeViewSelect>>", self.validate_selection)
+
+        def on_active(self):
+            print("Serial connection dialog active")
+            print(f"Searching for any of VIDs: {meshtastic.util.get_unique_vendor_ids()}")
+            self.ports_list.delete(*self.ports_list.get_children())
+            devices = meshtastic.util.detect_supported_devices()
+            if len(devices) > 0:
+                for device in devices:
+                    print(f"Found serial device: {device.name}")
+                    iid: str = self.ports_list.insert(
+                        "", tk.END, text=f"{device.name} ({device.device_class})", tags=("device",), open=True
+                    )
+                    for port in meshtastic.util.active_ports_on_supported_devices((device,), True):
+                        self.ports_list.insert(iid, tk.END, text=port, tags=("port",))
+            else:
+                for port in meshtastic.util.findPorts(True):
+                    self.ports_list.insert("", tk.END, text=port, tags=("port",))
+
+            self.ports_list.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+            self.ports_list_scroll.pack(side=tk.RIGHT, fill=tk.Y, expand=1)
+
+        def connect_device(self) -> Optional[MeshInterface]:
+            selection: tuple[str] = self.ports_list.selection()
+            if len(selection) == 1 and self.ports_list.tag_has("port", selection[0]):
+                iid: str = selection[0]
+                port: str = self.ports_list.item(iid, "text")
+                return meshtastic.serial_interface.SerialInterface(port), port
+
+            return None
+
+    class BLEInterfaceWidget(ttk.Frame):
         pass
+
+    class TCPInterfaceWidget(ttk.Frame):
+        ctype: str = "TCP Link"
+        def __init__(self, parent):
+            pass
